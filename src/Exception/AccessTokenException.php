90af726edb19ea6026422eb6ceb6cdee59745e9a
Emre Eromay <eeromay@axro.de>
2023-05-11 08:24:01 +0200

    AD-3672 fixed AccessTokenException

--- src/Exception/AccessTokenException.php
+++ src/Exception/AccessTokenException.php
@@ -6,5 +6,29 @@ use Exception;
 
 class AccessTokenException extends Exception
 {
+    private mixed $request;
+    private mixed $response;
 
+    public function __construct(
+        $message = null,
+        $request = null,
+        $response = null,
+    ) {
+        $message = $message ?: 'Invalid or missing AccessToken!';
+
+        parent::__construct($message);
+        $this->message = $message;
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function getRequest(): mixed
+    {
+        return $this->request;
+    }
+
+    public function getResponse(): mixed
+    {
+        return $this->response;
+    }
 }
